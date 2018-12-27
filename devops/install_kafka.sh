c56fa2f3dc6c1cb5b5d03edeec25f22615bc8bf3
spapp <stefan.papp@dxc.com>
2018-12-27 16:56:32 +0400

    added devops

--- devops/install_kafka.sh
+++ devops/install_kafka.sh
@@ -1,9 +1,11 @@
-#!/bin/sh
-apt-get update
-apt-get install -y wget openjdk-8-jdk
-cd /usr/lib/jvm
-ln -s java-8-openjdk-amd64 jdk
-cd /usr/local
-wget https://www-us.apache.org/dist/kafka/2.1.0/kafka_2.12-2.1.0.tgz
-tar -xvf kafka_2.12-2.1.0.tgz
-mv kafka_2.12-2.1.0 kafka
+#!/bin/bash
+# java is preinstalled
+
+# using 2.11 scala as still standard in ubuntu 18.10
+# mkdir opt
+wget https://www-us.apache.org/dist/kafka/2.1.0/kafka_2.11-2.1.0.tgz -P /tmp/
+tar -xvf /tmp/kafka_2.11-2.1.0.tgz
+mv /kafka_2.11-2.1.0 /opt/kafka
+
+echo -e "\nexport PATH=\$PATH:/opt/kafka/bin" >> /root/.bashrc
+echo -e "\nexport PATH=\$PATH:/opt/kafka/bin" >> /root/.zshrc
